b74eea3d31f6d4d9c1cb6cdf70e10dc724cbe9e2
yserbane <donggar97@naver.com>
2021-10-13 19:50:58 +0900

    [Solve] 💯 BOJ_1476.날짜 계산

--- Week/Week04/1476_oh.py
+++ Week/Week04/1476_oh.py
@@ -1,3 +1,4 @@
+# 다시 커밋
 import sys
 
 input = sys.stdin.readline
@@ -5,7 +6,6 @@ input = sys.stdin.readline
 e, s, m = map(int, input().split())
 
 n = 7981
-
 
 for i in range(1, n):
     if(i % 15 == (e % 15) and i % 28 == (s % 28) and i % 19 == (m % 19)):
