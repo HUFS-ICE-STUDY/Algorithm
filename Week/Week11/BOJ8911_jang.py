035442bab0ba12843ecd71a96296f3a137b9d7ae
jsh9611 <jangsh9611@naver.com>
2021-11-28 21:27:41 +0900

    [Solve] 💯 BOJ8911_거북이

---
+++ Week/Week11/BOJ8911_jang.py
@@ -0,0 +1,31 @@
+def answer(s):   
+    x,y = 499,499
+    state = 0   # 0: 위, 1: 오른쪽, 2: 아래, 3: 왼쪽
+    dic = { 0:(0,-1), 1:(1,0), 2:(0,1), 3:(-1,0) }
+    l,r,u,d = 499,499,499,499
+    for item in s:
+        if item == 'F':
+            tx,ty = dic[state]
+            x += tx
+            y += ty
+        elif item == 'B':
+            tx,ty = dic[state]
+            x -= tx
+            y -= ty
+        elif item == 'L': 
+            state = (state+4-1)%4
+        elif item == 'R':
+            state = (state+4+1)%4
+        
+        if x>r: r = x
+        if x<l: l = x
+        if y>d: d = y
+        if y<u: u = y
+
+    return (r-l)*(d-u)
+
+n = int(input())
+
+for i in range(n):
+    s = input()
+    print(answer(s))
